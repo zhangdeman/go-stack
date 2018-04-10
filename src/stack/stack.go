6a8283e06a99261a830ee1e116e2723e80bd4aeb
张德满 <2215508028@qq.com>
2018-04-10 22:39:46 +0800

    增加栈的逆序操作

--- src/stack/stack.go
+++ src/stack/stack.go
@@ -23,108 +23,121 @@ func MakeStack(size int64) Stack {
 /**
  * 栈是否已满,已用空间等于栈容量，即为栈满
  */
-func (instance *Stack) IsFull() bool {
-	return instance.size == instance.used
+func (stack *Stack) IsFull() bool {
+	return stack.size == stack.used
 }
 
 /**
  * 栈是否为空,已用空间为0即为空
  */
-func (instance *Stack) IsEmpty() bool {
-	return 0 == instance.used
+func (stack *Stack) IsEmpty() bool {
+	return 0 == stack.used
 }
 
 /**
  * 清空栈,硬清除，数据清空
  */
-func (instance *Stack) HardClear() {
-	instance.data = make([]interface{}, instance.size)
+func (stack *Stack) HardClear() {
+	stack.data = make([]interface{}, stack.size)
 }
 
 /**
  * 清空栈,软清除，数据不清空
  */
-func (instance *Stack) SoftClear() {
-	instance.used = 0
+func (stack *Stack) SoftClear() {
+	stack.used = 0
 }
 
 /**
  * 获取栈长度
  */
-func (instance *Stack) GetStackLength() int64 {
-	return instance.used
+func (stack *Stack) GetStackLength() int64 {
+	return stack.used
 }
 
 /**
  * 获取栈容量
  */
-func (instance *Stack) GetStackSize() int64 {
-	return instance.size
+func (stack *Stack) GetStackSize() int64 {
+	return stack.size
 }
 
 /**
  * 获取栈全部数据
  */
-func (instance *Stack) GetFullStackData() []interface{} {
-	return instance.data
+func (stack *Stack) GetFullStackData() []interface{} {
+	return stack.data
 }
 
 /**
  * 获取栈顶元素
  */
-func (instance *Stack) GetTopData() interface{}  {
-	return instance.data[instance.used - 1]
+func (stack *Stack) GetTopData() interface{}  {
+	return stack.data[stack.used - 1]
 }
 
 /**
  * 出栈操作
  */
-func (instance *Stack) Pop() (interface{}, int64, error) {
-	if instance.IsEmpty() {
+func (stack *Stack) Pop() (interface{}, int64, error) {
+	if stack.IsEmpty() {
 		err := errors.New("栈已为空，无数据")
 		fmt.Println("栈已为空，无数据")
 		return nil, 0, err
 	}
-	data := instance.data[instance.used]
-	afterPop := instance.data[0:instance.used]
-	instance.used = instance.used - 1
-	instance.data = afterPop
-	return data, instance.used, nil
+	data := stack.data[stack.used]
+	afterPop := stack.data[0:stack.used]
+	stack.used = stack.used - 1
+	stack.data = afterPop
+	return data, stack.used, nil
 }
 
 /**
  * 入栈操作
  */
-func (instance *Stack) Push(data interface{}) (int64, error) {
-	if instance.IsFull() {
-		instance.size = instance.size * 2
+func (stack *Stack) Push(data interface{}) (int64, error) {
+	if stack.IsFull() {
+		stack.size = stack.size * 2
 		fmt.Println("栈已满，无法压入数据, 自动扩容")
 	}
-	afterPush := append(instance.data, data)
-	instance.data = afterPush
-	instance.used = instance.used + 1
-	return instance.used, nil
+	afterPush := append(stack.data, data)
+	stack.data = afterPush
+	stack.used = stack.used + 1
+	return stack.used, nil
 }
 
 /**
  * 修改指定索引值
  */
-func (instance *Stack) ModifyIndexValue(index int64, data interface{}) (interface{}, error) {
-	if index > instance.size {
+func (stack *Stack) ModifyIndexValue(index int64, data interface{}) (interface{}, error) {
+	if index > stack.size {
 		fmt.Println("索引越界")
 		return  nil, errors.New("索引越界")
 	}
-	instance.data[index] = data
+	stack.data[index] = data
 	return data, nil
 }
 
 /**
  * 获取指定索引值
  */
-func (instance *Stack) GetIndexValue(index int64, data interface{}) (interface{}, error) {
-	if index > instance.size || index < 0 {
+func (stack *Stack) GetIndexValue(index int64, data interface{}) (interface{}, error) {
+	if index > stack.size || index < 0 {
 		fmt.Println("索引越界")
 		return  nil, errors.New("索引越界")
 	}
-	return instance.data[index], nil
+	return stack.data[index], nil
+}
+
+/**
+ * 逆转一个栈
+ * @return slice
+ */
+func (stack *Stack) ReversalStack() []interface{} {
+	stackLen := len(stack.data)
+	reversalStack := make([]interface{}, stackLen)
+	for index := stackLen; index > 0; index-- {
+		reversalStack = append(reversalStack, stack.data[index])
+	}
+	return reversalStack
 }
