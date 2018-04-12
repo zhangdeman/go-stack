c1e08dcc66014b9f349e4e56cfe5343a2c00c0b2
zhangdeman <zhangdeman@didichuxing.com>
2018-04-12 17:00:02 +0800

    增加队列测试

--- Test_queue.go
+++ Test_queue.go
@@ -30,4 +30,20 @@ func main()  {
 		}
 		fmt.Println("data ",data)
 	}
+	queue.Push("q")
+	queue.Push("w")
+	queue.Push("e")
+	data, _ := queue.Pop()
+	fmt.Println("data ",data)
+	queue.Push("a")
+	queue.Push("s")
+	queue.Push("d")
+	for  {
+		data, err := queue.Pop()
+		if err != nil {
+			fmt.Println("队列数据为空")
+			break
+		}
+		fmt.Println("data ",data)
+	}
 }
